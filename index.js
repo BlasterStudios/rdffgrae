7e7eded378c7ebae070e6acd8f02c381add30e72
MR_Blaster <73756718+BlasterStudios@users.noreply.github.com>
2021-03-12 23:23:42 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function dashboard() {
-
-    health.insertAdjacentHTML('beforeEnd', ' yes')
-
+//HTML:
+function myFunction() {
+    var x = document.getElementById("loginn").elements[0].value;
+    document.getElementById("demo").innerHTML = x;
+    alert(x)
 }
+console.log(x)
+alert(x)
+
+function dashboard() {
+    document.getElementById("health").innerHTML = "Bot health: no";
+}
+setInterval(async function() {
+    dashboard();
+}, 0.1 * 60000);
+
+//health.insertAdjacentHTML('beforeEnd', ' yes')
